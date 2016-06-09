61cd46d4a79e92f9d38b86a0ec5c41a31204c123
Aveza <mikkottis@hotmail.com>
2016-06-09 11:31:26 +0300

    Esmane broneerimissüsteem

---
+++ konf.php
@@ -0,0 +1,10 @@
+﻿<?php
+	$baasiaadress="localhost";
+	$baasikasutaja="if13";
+	$baasiparool="ifikad";
+	$baasinimi="if13_mikkottis";
+	$yhendus=new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi);
+	
+	if($yhendus === false){
+		die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
