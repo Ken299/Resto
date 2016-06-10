b7a9397ff357626fe2f647a5cb4d9ccf7b520722
Aveza <mikkottis@hotmail.com>
2016-06-10 13:19:40 +0300

    24h jooksul ei saa broneerida

--- broneering.php
+++ broneering.php
@@ -16,19 +16,81 @@
 				});
 			});
 			
-			function showAvailability(str){
-				if (window.XMLHttpRequest) {
-					xmlhttp = new XMLHttpRequest();
+			function showAvailability(){
+				rawDate = $("#datepicker")[0].value;
+				if (!rawDate || rawDate == "") {
+					return;
 				} else {
-					xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+					compareDates();
+					checkIfFull();
 				}
-				xmlhttp.onreadystatechange = function() {
-					if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-						document.getElementById("txt_seats").innerHTML = "Vabu kohti: "+xmlhttp.responseText;
+				
+				function compareDates(){
+					var currentDate = new Date();
+					var selectedDate = new Date(Date.parse(rawDate));
+					
+					var curHour = parseInt(currentDate.getHours());
+					var curMin = parseInt(currentDate.getMinutes());
+					var selHour = parseInt($("#tunnid")[0].value);
+					var selMin = parseInt($("#minutid")[0].value);
+					
+					// Et päevade kontrollimisel loeks ainult päev, kuu ja aasta.
+					currentDate.setHours(0,0,0,0);
+					selectedDate.setHours(0,0,0,0);
+					
+					if (selectedDate.getTime() < currentDate.getTime()) {
+						$("#error_date").text("Valitud päev on juba möödunud");
+						$("#btn_submit")[0].disabled = true;
+					} else if (selectedDate.getTime() > currentDate.getTime() + 86400000) {
+						$("#error_date").empty();
+						$("#btn_submit")[0].disabled = false;
+					} else if (selectedDate.getTime() == currentDate.getTime() + 86400000) {
+						if (curHour < selHour) {
+							$("#error_date").empty();
+							$("#btn_submit")[0].disabled = false;
+						} else if (curHour > selHour) {
+							$("#error_date").text("Broneerida saab ainult 24h enne valitud aega.");
+							$("#btn_submit")[0].disabled = true;
+						} else {
+							if (curMin < selMin) {
+								$("#error_date").empty();
+								$("#btn_submit")[0].disabled = false;
+							} else {
+								$("#error_date").text("Broneerida saab ainult 24h enne valitud aega.");
+								$("#btn_submit")[0].disabled = true;
+							}
+						}
+					} else {
+						$("#btn_submit")[0].disabled = true;
+						if (curHour < selHour) {
+							$("#error_date").text("Broneerida saab ainult 24h enne valitud aega.");
+						} else if (curHour > selHour) {
+							$("#error_date").text("Valitud aeg on juba möödunud.");
+						} else {
+							if (curMin < selMin) {
+								$("#error_date").text("Broneerida saab ainult 24h enne valitud aega.");
+							} else {
+								$("#error_date").text("Valitud aeg on juba möödunud.");
+							}
+						}
 					}
-				};
-				xmlhttp.open("GET","getdates.php?q="+str,true);
-				xmlhttp.send();
+				}	
+				
+				function checkIfFull(){
+					if (window.XMLHttpRequest) {
+						xmlhttp = new XMLHttpRequest();
+					} else {
+						xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+					}
+					xmlhttp.onreadystatechange = function() {
+						if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+							document.getElementById("txt_seats").innerHTML = "Vabu kohti: "+xmlhttp.responseText;
+						}
+					};
+					xmlhttp.open("GET","getdates.php?q="+rawDate,true);
+					xmlhttp.send();
+				}
+
 			}
 			
 			$(document).ready(function(){
@@ -79,10 +141,10 @@
 			<p>Telefon:</p>
 			<input type="number" name="telefon" id="telefon" required>
 			<p>Kuupäev:</p>
-			<input type="text" onchange="showAvailability(this.value)" name="kuupaev" id="datepicker" required>
+			<input type="text" onchange="showAvailability()" name="kuupaev" id="datepicker" required>
 			<p id="txt_seats"></p>
 			<p>Aeg:</p>
-			<select name="tunnid" id="tunnid">
+			<select name="tunnid" id="tunnid" onchange="showAvailability()">
 				<option value="8">8</option>
 				<option value="9">9</option>
 				<option value="10">10</option>
@@ -98,18 +160,17 @@
 				<option value="20">20</option>
 				<option value="21">21</option>
 			</select> :
-			<select name="minutid" id="minutid">
+			<select name="minutid" id="minutid" onchange="showAvailability()">
 				<option value="00">00</option>
-				<option value="10">10</option>
-				<option value="20">20</option>
+				<option value="15">15</option>
 				<option value="30">30</option>
-				<option value="40">40</option>
-				<option value="50">50</option>
+				<option value="45">45</option>
 			</select>
+			<p id="error_date"></p>
 			<p>Lisainfo:</p>
 			<textarea rows="4" cols="40" name="lisainfo" id="lisainfo"></textarea></br></br>
 			
-			<input type="submit" value="Broneeri">
+			<input type="submit" id="btn_submit" value="Broneeri">
 			<p id="txt_result"></p>
 		</form>
 	</body>
