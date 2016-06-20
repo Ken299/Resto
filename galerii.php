2b2ae8e3ee83bf9b839ef4e7264ccb829f100778
Sander Leetus <leetussa@greeny.cs.tlu.ee>
2016-06-20 20:13:11 +0300

    galerii muudatused

--- galerii.php
+++ galerii.php
@@ -1,4 +1,4 @@
-<?php
+ï»¿<?php
 	require_once('php/functions.php');
 	//if(isset($_SESSION["rights"])){
 		/*if(($_SESSION["rights"])!=1){
@@ -25,17 +25,11 @@
 </head>
 <body>
 	<script>
-		$(document).ready(function(){
-			
-		});
-		
-	</script>
-	<script>
 	$(document).ready(function() {
 	$('a[data-confirm]').click(function(ev) {
 		var href = $(this).attr('href');
 		if (!$('#dataConfirmModal').length) {
-			$('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
+			$('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">Ã—</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
 		} 
 		$('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
 		$('#dataConfirmOK').attr('href', href);
@@ -46,31 +40,29 @@
 </script>
 	<nav class="navbar navbar-inverse">
 	<div class="navbar-header">
-	<p><a href="admin.php"><img src="img/relatedposts/manna.png" width="50px" height="50px"></a><span style="color: white;" class="MVD"> Manna Va Doosa kontohaldus</span></p>
+	<p><a href="galerii.php"><img src="img/relatedposts/manna.png" width="50px" height="50px"></a><span style="color: white;" class="MVD"> Manna Va Doosa kontohaldus</span></p>
 	</div>
-	<p align="right" style="margin-right: 40px; margin-top: 15px;"class="menu-item"><a href="php/logout.php" class="btn btn-restoran">Logi välja</a></p>
+	<p align="right" style="margin-right: 40px; margin-top: 15px;"class="menu-item"><a href="php/logout.php" class="btn btn-restoran">Logi vÃ¤lja</a></p>
 	</nav>
 	<br><br><br>
 	<ul class="nav nav-tabs" id="myTab">
-		<li class="active"><a href="#tab1">Kasutajad</a></li>
-		<li><a href="#tab2">Uus kasutaja</a></li>
+		<li class="active"><a href="#tab1">Galerii piltide lisamine</a></li>
 	</ul>
 	<div class="tab-content">
 	<br>
 	<div id="tab1" class="tab-pane fade in active">
-	<h2 align="center" style="color: white;">Kontode haldamine</h2>
+	<h2 align="center" style="color: white;">Lisa pilte</h2>
 	<br>
-	<div class="col-md-2 col-md-offset-5" style="background-color: rgba(0,0,0, .3); border-radius: 20px;">
+	<div class="col-md-3 col-md-offset-5" style="background-color: rgba(0,0,0, .3); border-radius: 20px;">
 		<br>
-		<h1>FUCK YOU</h1>
 		<form action="php/create_photo_gallery.php" method="post" enctype="multipart/form-data">
 			<table width="100%">
 				<tr>
-					<td>Select Photo (one or multiple):</td>
+					<td style="color:white">Select Photo (one or multiple):</td>
 					<td><input type="file" name="files[]" multiple /></td>
 				</tr>
 				<tr>
-					<td colspan="2" align="center">Note: Supported image format: .jpeg, .jpg, .png, .gif</td>
+					<td colspan="2" align="center" style="color:white">Note: Supported image format: .jpeg, .jpg, .png, .gif</td>
 				</tr>
 				<tr>
 					<td colspan="2" align="center"><input type="submit" value="Create Gallery" id="selectedButton"/></td>
@@ -80,9 +72,6 @@
 	</div>
 	</div>
 	<div id="tab2" class="tab-pane fade">
-	<div class="col-md-2 col-md-offset-5" style="background-color: rgba(0,0,0, .3); border-radius: 20px;">
-		
-	</div>
 	</div>
 	</div>
 	<!-- tabi salvestamine refreshil -->
@@ -101,63 +90,8 @@
 		// on load of the page: switch to the currently selected tab
 		var hash = window.location.hash;
 		$('#myTab a[href="' + hash + '"]').tab('show');
-		
-		/*var dir = "php/images2/image_uploads";
-		var fileextension = ".jpg";
-		$.ajax({
-			//This will retrieve the contents of the folder if the folder is configured as 'browsable'
-			url: dir,
-			success: function (data) {
-				//Lsit all png file names in the page
-				$(data).find("a:contains(" + fileextension + ")").each(function () {
-					var filename = this.href.replace(window.location.host, "").replace("http:///~leetussa/Suvepraktika/Resto", "");
-					$("#pildid").append($("<img src=" + dir + filename + "></img>"));
-				});
-			}
-		});*/
 		</script>
-		<div class="highslide-gallery" id="pildid" align='center'>
-		<?php
-
-   $files = glob("php/images2/image_uploads/*.*");
-
-  for ($i=0; $i<count($files); $i++)
-
-{
-
-$image = $files[$i];
-$supported_file = array(
-    'gif',
-    'jpg',
-    'jpeg',
-    'png'
-);
-
-$ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
-
-if (in_array($ext, $supported_file)) {
-    //print $image ."<br />";
-	if($i==0){
-		$id = $i;
-		echo '<a id="'.$i.'" href="'.$image .'" class="highslide" onclick="return hs.expand(this, {slideshowGroup: 1})" alt="Random image" ><img src="'.$image.'" alt="Random image" title="Click to enlarge" width="304" height=236"/> </a>'."<br /><br />";
-		echo '<div class="highslide-caption" ></div>';
 		
-	}
-	
-	else{
-		echo '<div class="hidden-container">';
-		echo '<a href="'.$image.'" class="highslide" onclick="return hs.expand(this, {thumbnailId: '.$id.', slideshowGroup: 1})"></a>';
-		echo '<div class="highslide-caption"></div>';
-	}
-   
-} else {
-    continue;
- }
-
-}
-
-?>
-	</div>
 	
 </body>
 </html>
